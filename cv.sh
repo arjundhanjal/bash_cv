41e3870d0e214194f469c83f2e312614fe42fcb7
Arjun Dhanjal <hello@arjundhanjal.com>
2015-04-11 13:57:05 -0400

    updated filepaths

--- cv.sh
+++ cv.sh
@@ -15,5 +15,5 @@ echo "This script is designed to upload CVs to the landfill at ArjunDhanjal.com.
 echo "This script will also update the symlink located at ${FGGRN}http://landfill.arjundhanjal.com/AD-CV_current.pdf ${NORMAL}"
 echo "To proceed, please enter the version number of the CV you'd like to upload: \c"
 read vnum
-rsync -e 'ssh -p 2222' /Users/arjun/Dropbox/CurriculumVitae/AD-CV_v${vnum}.pdf adhanjal@backend.arjundhanjal.com:~/landfill/.CV/AD-CV_v${vnum}.pdf
-ssh adhanjal@backend.arjundhanjal.com -p 2222 "cd landfill/ ; ln -fs .CV/AD-CV_v${vnum}.pdf AD-CV_current.pdf"
+rsync -e 'ssh -p 2222' /Users/arjun/Dropbox/CurriculumVitae/AD-resume_v${vnum}.pdf adhanjal@backend.arjundhanjal.com:~/landfill/.CV/AD-resume_v${vnum}.pdf
+ssh adhanjal@backend.arjundhanjal.com -p 2222 "cd landfill/ ; ln -fs .CV/AD-resume_v${vnum}.pdf AD-resume_current.pdf"
